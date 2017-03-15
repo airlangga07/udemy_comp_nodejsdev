736808474dcad31faaa9b9347776e049681ed971
Mikael Airlangga Trianggoro <mikael.airlangga@gmail.com>
2017-03-15 22:30:50 +0800

    Node.js Fundamentals: Using 3rd Party Modules

--- nodejs_fundamentals/notes-node/app.js
+++ nodejs_fundamentals/notes-node/app.js
@@ -6,23 +6,33 @@ const fs = require('fs');
 // add the Operating System info module
 const os = require('os');
 
-// get the user object
-var user = os.userInfo();
+// requiring lodash
+const _ = require('lodash');
 
 // add our built in file
 const notes = require('./notes.js');
 
-// retrieve the functions
+// using the lodash functions
+// this one is checking wheter the supplied argument is string or not and return a bool
+console.log(_.isString(true));
+console.log(_.isString('Mikael'));
+
+// this one return an array with no duplicates
+var filteredArray = _.uniq(['elang', 'elang', 1, 2, 3, 4, 'elang', 2, 4, 5, 'test']);
+console.log(filteredArray);
+
+// retrieve the our built in functions
 var res = notes.addNote();
 console.log(res);
-
 var add = notes.add(1, 2);
 console.log(`Result: ${add}`);
 
 // create a file and append text inside it, in here we use the user and get the username property and add it to the file
 // Javascript es6 offers the template string where you can format your string and insert variables just like ruby, just use the `` 
-// fs.appendFile('greetings.txt', `Hello ${user.username}! you are ${notes.age}.`, function (err) {
-//   if (err) {
-//     console.log('Unable to write to file');
-//   }
-// });
+// get the user object
+var user = os.userInfo();
+fs.appendFile('greetings.txt', `Hello ${user.username}! you are ${notes.age}.`, function (err) {
+  if (err) {
+    console.log('Unable to write to file');
+  }
+});
