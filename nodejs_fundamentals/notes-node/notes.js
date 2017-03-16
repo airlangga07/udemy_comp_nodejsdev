2e0bb80b1dcce2486194bc352fec6f3eccac0e51
Mikael Airlangga Trianggoro <mikael.airlangga@gmail.com>
2017-03-16 21:46:34 +0800

    Node.js Fundamentals: Adding and Saving Notes

--- nodejs_fundamentals/notes-node/notes.js
+++ nodejs_fundamentals/notes-node/notes.js
@@ -1,7 +1,41 @@
 console.log("Starting notes.js");
 
+const fs = require('fs');
+
 var addNote = (title, body) => {
-  console.log("Adding note", title, body);
+  var notes = [];
+  var note  = {
+    title: title, 
+    body: body
+  };
+
+  try {
+    var notesString = fs.readFileSync('notes-data.json');
+    notes = JSON.parse(notesString);
+  } catch (e) {
+
+  }
+
+  // ES 2015 or ES5 would do something like this
+  //    var duplicateNotes = notes.filter(function(note) {
+  //      if (note.title === title) {
+  //        return true;
+  //      }
+  //      return false;
+  //    });
+  // 
+  // ES 2016 or ES6 would do the arrow function like this
+  //    var duplicateNotes = notes.filter((note) => {
+  //       return note.title === title;
+  //     });
+  // But you can simplify if you only have one line of return call to be like this
+  var duplicateNotes = notes.filter((note) => note.title === title);
+
+  if (duplicateNotes.length === 0) {
+    notes.push(note);
+    fs.writeFileSync('notes-data.json', JSON.stringify(notes));
+    console.log("Adding note", title, body);
+  }
 };
 
 var getAll = () => {
