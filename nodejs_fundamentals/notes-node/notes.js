7467f392ef61c1e2f674e7784c2745b81f2dbdeb
Mikael Airlangga Trianggoro <mikael.airlangga@gmail.com>
2017-03-16 22:17:20 +0800

    Node.js Fundamentals: Refactoring for Reusability

--- nodejs_fundamentals/notes-node/notes.js
+++ nodejs_fundamentals/notes-node/notes.js
@@ -2,19 +2,25 @@ console.log("Starting notes.js");
 
 const fs = require('fs');
 
+var fetchNotes = () => {
+  try {
+    var notesString = fs.readFileSync('notes-data.json');
+    return JSON.parse(notesString);
+  } catch (e) {
+    return [];
+  }
+};
+
+var saveNotes = (notes) => {
+  fs.writeFileSync('notes-data.json', JSON.stringify(notes));
+};
+
 var addNote = (title, body) => {
-  var notes = [];
+  var notes = fetchNotes();
   var note  = {
     title: title, 
     body: body
   };
-
-  try {
-    var notesString = fs.readFileSync('notes-data.json');
-    notes = JSON.parse(notesString);
-  } catch (e) {
-
-  }
 
   // ES 2015 or ES5 would do something like this
   //    var duplicateNotes = notes.filter(function(note) {
@@ -33,8 +39,8 @@ var addNote = (title, body) => {
 
   if (duplicateNotes.length === 0) {
     notes.push(note);
-    fs.writeFileSync('notes-data.json', JSON.stringify(notes));
-    console.log("Adding note", title, body);
+    saveNotes(notes);
+    return note;
   }
 };
 
