d1541e912b98a831d956a285f2a3e5cd2b5fcaa9
Mikael Airlangga Trianggoro <mikael.airlangga@gmail.com>
2017-03-20 22:39:31 +0800

    Testing Your App: Using an Assertion Library (refactor)

--- node-tests/utils/utils.test.js
+++ node-tests/utils/utils.test.js
@@ -3,29 +3,19 @@ const utils = require("./utils");
 
 it('should add two numbers', () => {
   var res = utils.add(33, 11);
-
-  // if(res !== 44) {
-  //   throw new Error(`Expected 44, but got ${res}`);
-  // }
-  // change to 
   expect(res).toBe(44).toBeA('number');
 });
 
 it('should square a number', () => {
   var res = utils.square(4);
-
-  // if(res !== 16) {
-  //   throw new Error(`Expected 16, but got ${res}`);
-  // }
-  // change to
   expect(res).toBe(16).toBeA('number');
 });
 
-it('should expect some values', () => {
+// it('should expect some values', () => {
   // expect(12).toNotBe(11);
   // expect({name: 'Andrew'}).toEqual({name: 'Andrew'});
   // expect([2,3,4]).toInclude(2);
-});
+// });
 
 it('should verify first and last names are set', () => {
   var res = utils.setName({ location: "Singapore", age: "27" }, "Mikael Airlangga");
