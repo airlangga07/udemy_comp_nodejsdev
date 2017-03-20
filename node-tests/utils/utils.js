6f29a368fe24a4004f63fac193b608e430b78129
Mikael Airlangga Trianggoro <mikael.airlangga@gmail.com>
2017-03-20 22:29:24 +0800

    Testing Your App: Using an Assertion Library

--- node-tests/utils/utils.js
+++ node-tests/utils/utils.js
@@ -1,3 +1,11 @@
 module.exports.add = (a, b) => a + b;
 
 module.exports.square = (x) => x * x;
+
+module.exports.setName = (user, fullName) => {
+  var names = fullName.split(" ");
+  user.firstname = names[0];
+  user.lastname = names[1];
+
+  return user;
+}
--- node-tests/utils/utils.test.js
+++ node-tests/utils/utils.test.js
@@ -1,17 +1,37 @@
+const expect = require("expect");
 const utils = require("./utils");
 
 it('should add two numbers', () => {
   var res = utils.add(33, 11);
 
-  if(res !== 44) {
-    throw new Error(`Expected 44, but got ${res}`);
-  }
+  // if(res !== 44) {
+  //   throw new Error(`Expected 44, but got ${res}`);
+  // }
+  // change to 
+  expect(res).toBe(44).toBeA('number');
 });
 
 it('should square a number', () => {
   var res = utils.square(4);
 
-  if(res !== 16) {
-    throw new Error(`Expected 16, but got ${res}`);
-  }
+  // if(res !== 16) {
+  //   throw new Error(`Expected 16, but got ${res}`);
+  // }
+  // change to
+  expect(res).toBe(16).toBeA('number');
+});
+
+it('should expect some values', () => {
+  // expect(12).toNotBe(11);
+  // expect({name: 'Andrew'}).toEqual({name: 'Andrew'});
+  // expect([2,3,4]).toInclude(2);
+});
+
+it('should verify first and last names are set', () => {
+  var res = utils.setName({ location: "Singapore", age: "27" }, "Mikael Airlangga");
+
+  expect(res).toInclude({
+    firstname: "Mikael",
+    lastname: "Airlangga"
+  });
 });
