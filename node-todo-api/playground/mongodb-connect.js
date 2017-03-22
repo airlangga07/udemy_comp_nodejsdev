0870586dae61ce06573f305ebf8170a44b0a920f
Mikael Airlangga Trianggoro <mikael.airlangga@gmail.com>
2017-03-22 19:04:38 +0800

    MongoDB, Mongoose and REST API: Connecting to Mongo and Writing Data

--- node-todo-api/playground/mongodb-connect.js
+++ node-todo-api/playground/mongodb-connect.js
@@ -6,5 +6,28 @@ MongoClient.connect("mongodb://localhost:27017/TodoApp", (err, db) => {
   }
   console.log("connected to mongodb server.")
 
+  // create new collection, then insert new data
+  // db.collection('Todos').insertOne({
+  //   text: "Something to do",
+  //   completed: false
+  // }, (err, result) => {
+  //   if (err) {
+  //     return console.log("Unable to insert todo", err);
+  //   }
+  //   console.log(JSON.stringify(result.ops, undefined, 2));
+  // });
+
+  // insert new doc into users
+  db.collection('Todos').insertOne({
+    name: "Mikael Airlangga",
+    age: 27,
+    location: "singapore"
+  }, (err, result) => {
+    if (err) {
+      return console.log("Unable to insert data", err);
+    }
+    console.log(JSON.stringify(result.ops, undefined, 2));
+  })
+
   db.close();
 });
