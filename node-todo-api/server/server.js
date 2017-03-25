4b8786cc49a56668f25daa1dbff78240327df9c8
Mikael Airlangga Trianggoro <mikael.airlangga@gmail.com>
2017-03-25 12:55:07 +0800

    MongoDB, Mongoose and REST API: Updating a Resource - PATCH /todos/:id

--- node-todo-api/server/server.js
+++ node-todo-api/server/server.js
@@ -1,6 +1,6 @@
-var express = require("express");
-var bodyParser = require("body-parser");
-
+const express = require("express");
+const bodyParser = require("body-parser");
+const _ = require("lodash");
 
 var { mongoose } = require("./db/mongoose");
 var { Todo } = require("./models/todo");
@@ -8,7 +8,7 @@ var { User } = require("./models/user");
 var { ObjectID } = require("mongodb");
 
 var app = express();
-const port = process.env.PORT || 3000;
+var port = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
 
@@ -56,6 +56,35 @@ app.delete("/todos/:id", (req, res) => {
     if (!todo) {
       return res.status(404).send();
     }
+    res.status(200).send({todo});
+  }).catch((e) => {
+    res.status(400).send();
+  })
+})
+
+app.patch("/todos/:id", (req, res) => {
+  var id = req.params.id;
+  // pull off the propoerties from the body using .pick, it takes an object, and takes an array of the body if they exist
+  var body = _.pick(req.body, ['text', 'completed']);
+
+  if(!ObjectID.isValid(id)) {
+    return res.status(404).send();
+  }
+
+  // update the completedAt if completed changed to true
+  // is body.completed is boolean and true
+  if(_.isBoolean(body.completed) && body.completed) {
+    body.completedAt = new Date().getTime()
+  } else {
+    body.completed = false;
+    body.completedAt = null;
+  }
+
+  Todo.findByIdAndUpdate(id, {$set: body}, {new: true}).then((todo) => {
+    if (!todo) {
+      return res.status(404).send();
+    }
+
     res.status(200).send({todo});
   }).catch((e) => {
     res.status(400).send();
