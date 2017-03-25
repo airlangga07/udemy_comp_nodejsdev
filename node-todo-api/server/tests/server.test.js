15a7d62b9bfca7a2fd6a5efef1dc63bae3bdaa57
Mikael Airlangga Trianggoro <mikael.airlangga@gmail.com>
2017-03-25 12:33:57 +0800

    MongoDB, Mongoose and REST API: Testing DELETE /todos/:id

--- node-todo-api/server/tests/server.test.js
+++ node-todo-api/server/tests/server.test.js
@@ -100,3 +100,41 @@ describe("GET /todos/:id", () => {
       .end(done);
   });
 })
+
+describe("DELETE /todos/:id", () => {
+  it("Should remove a todo", (done) => {
+    var hexID = todos[1]._id.toHexString();
+    request(app)
+      .delete(`/todos/${hexID}`)
+      .expect(200)
+      .expect((res) => {
+        expect(res.body.todo._id).toBe(hexID)
+      })
+      .end((err, res) => {
+        if (err) {
+          return done(err);
+        }
+
+        Todo.findById(hexID).then((todo) => {
+          expect(todo).toNotExist();
+          done();
+        }).catch((e) => done(e));
+      })
+  });
+
+  it("Should return a 404 if todo not found", (done) => {
+    var invalid_id = new ObjectID().toHexString();
+    request(app)
+      .delete(`/todos/${ invalid_id }`)
+      .expect(404)
+      .end(done);
+  });
+
+  it("Should return a 404 for non object ID", (done) => {
+    var invalid_url = "/todos/123";
+    request(app)
+      .delete(invalid_url)
+      .expect(404)
+      .end(done);
+  });
+})
