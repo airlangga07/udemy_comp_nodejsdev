95f638c6abbbf2143b43d4053f0bf5864dd21147
Mikael Airlangga Trianggoro <mikael.airlangga@gmail.com>
2017-03-25 22:45:01 +0800

    Security and Auth: Hashing Passwords

--- node-todo-api/playground/hashing.js
+++ node-todo-api/playground/hashing.js
@@ -1,18 +1,41 @@
 const { SHA256 } = require("crypto-js");
 const jwt = require("jsonwebtoken");
+const bcrypt = require("bcryptjs");
 
-var data = {
-  id: 10
-}
+
+// USING BCRYPT
+var password = '123abc!';
+
+// generate hashed password
+// bcrypt.genSalt(10, (err, salt) => {
+//   bcrypt.hash(password, salt, (err, hash) => {
+//     console.log(hash);
+//   });
+// });
+
+var hashedPassword = '$2a$10$ZaK2eRkmAOIHUa.8ExSJ.uHcyAYxuDApZrNH4T2FcVxh4EJiXigOm';
+
+// compare the password
+bcrypt.compare(password, hashedPassword, (err, res) => {
+  console.log(res);
+})
+
+// USING JWT
+// var data = {
+//   id: 10
+// }
 
 // takes the object and signs it, hash and it returns the token value
-var token = jwt.sign(data, "123abc");
-console.log(token);
+// var token = jwt.sign(data, "123abc");
+// console.log(token);
 
 // takes that token and the secret, and make sure the data not manipulated
-var decodedToken = jwt.verify(token, "123abc");
-console.log(decodedToken);
+// var decodedToken = jwt.verify(token, "123abc");
+// console.log(decodedToken);
 
+
+
+// MANUAL HASH
 // var message = "I am user number 3";
 // var hash = SHA256(message).toString();
 
