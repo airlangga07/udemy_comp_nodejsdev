37ecb14aa187667ac318861e8fffd08c566bcacc
Mikael Airlangga Trianggoro <mikael.airlangga@gmail.com>
2017-03-26 16:28:46 +0800

    Security and Auth: Seeding Test Database With Users

--- node-todo-api/server/tests/server.test.js
+++ node-todo-api/server/tests/server.test.js
@@ -3,25 +3,10 @@ const request = require("supertest");
 const { ObjectID } = require("mongodb");
 const { app } = require("./../server");
 const { Todo } = require("./../models/todo");
+const { todos, populateTodos, users, populateUsers } = require("./seed/seed");
 
-
-const todos = [{
-  _id: new ObjectID(),
-  text: "First test todo",
-  completed: false,
-  completedAt: 333
-}, {
-  _id: new ObjectID(),
-  text: "Second test todo",
-  completed: false,
-  completedAt: 333
-}];
-
-beforeEach((done) => {
-  Todo.remove({}).then(() => {
-    return Todo.insertMany(todos);
-  }).then(() => done());
-});
+beforeEach(populateUsers);
+beforeEach(populateTodos);
 
 describe("POST /todos", () => {
   it('Should create a new todo', (done) => {
